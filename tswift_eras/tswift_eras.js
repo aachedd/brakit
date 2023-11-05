2550f6464b6a2f7a5800728d12f2a91417f0ddad
aachedd <acheddie1125@gmail.com>
2023-11-05 15:29:09 -0500

    updating tswift to eras tour songs

---
+++ tswift_eras/tswift_eras.js
@@ -0,0 +1,38 @@
+/*---Hiding the two popups until the page loads---*/
+$(".modal").hide();
+
+/*---start of modal and popup function---*/
+$(document).ready(function(){
+    $(function(){
+        var overlay = $('<div id="overlay"></div>');
+        overlay.show();
+        overlay.appendTo(document.body);
+        $(".modal").show();
+
+        $(".confirm").click(function(){
+            $(".modal").hide();
+            event.preventDefault();
+            $("#defaultOpen").trigger('click');
+            /*Work on this to try to style the block*/
+            $("#defaultOpen").css("background-color", "#00B2CA");
+            overlay.appendTo(document.body).remove();
+            return false;
+        });
+    });
+});
+/*---end of popup function---*/
+
+/*---start logic to grab data from google sheet---*/
+var gak = config.gak;
+var gsid = config.gsid;
+
+$.getJSON("https://sheets.googleapis.com/v4/spreadsheets/" + gsid + "/values/Main?alt=json&key=" + gak, function (data) {
+  var i;
+  for (i = 1; i < 65; i++) {
+    var seed = data.values[i]['28'];
+    var descrip = data.values[i]['29'];
+    $("#seed" + i).val(i+'. ' + seed);
+    document.getElementById("seed" + i).title = descrip;
+                         }
+});
+/*---end logic to grab data from google sheet---*/
